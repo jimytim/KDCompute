9de03b46b045941391f783fc3f699e515e8d2362
tkheyrkhah <timothee.kheyrkhah@nist.gov>
2019-07-09 23:37:22 -0400

    Added logger creation helper function

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,40 @@
 import sys
+import logging 
 
 from PySide2.QtCore import QDateTime, QTimeZone
 from PySide2.QtWidgets import QApplication
 
 from main_window import MainWindow
 from stats import StatTab
+
+def create_logger(loglevel="INFO"):
+    """Create a logger with the provided log level
+
+    Args:
+        loglevel (str): loglevel string for the console -> 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
+    """
+    try:
+        numeric_console_loglevel = getattr(logging, loglevel.upper())
+    except AttributeError as e:
+        print("LoggingError: Invalid logLevel -> {}".format(e))
+        sys.exit(1)
+
+
+    logger = logging.getLogger('root')
+    # logger.setLevel(logging.DEBUG)
+
+    consoleLogger = logging.StreamHandler(stream=sys.stdout)
+    # consoleFormatter = logging.Formatter("{name:<5} - {levelname} - {message}", style='{')
+    consoleFormatter = logging.Formatter("{asctime}|{name:<5}|{levelname:^9} - {message}", datefmt='%H:%M:%S', style='{')
+    consoleLogger.setLevel(numeric_console_loglevel)
+    consoleLogger.setFormatter(consoleFormatter)
+    logger.addHandler(consoleLogger)
+         
+    # Silence the matplotlib logger
+    mpl_logger = logging.getLogger("matplotlib")
+    mpl_logger.setLevel(logging.WARNING)
+
+    return logger
 
 if __name__ == "__main__":
 
